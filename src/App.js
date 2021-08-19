96159816849132e02f4d4e68a794cb8aaab200b2
Luke Molloy <lukemolloy@Lukes-MacBook-Pro.local>
2021-08-19 14:06:50 +0100

    Updated styles, created searchbox component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search-box/search-box";
 
 class App extends React.Component {
   constructor() {
@@ -18,6 +19,10 @@ class App extends React.Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) => {
@@ -26,14 +31,10 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monsters"
-          onChange={(e) =>
-            this.setState({ searchField: e.target.value }, () =>
-              console.log(this.state.searchField)
-            )
-          }
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
